7abff82a51ecfc6e5816052653ace1f635b7c239
Richard Jacobsen <richard.jacobsen@vividimpact.com>
2024-03-13 16:51:27 -0400

    work in progress

--- resources/js/Pages/Resume/Partials/Skill.vue
+++ resources/js/Pages/Resume/Partials/Skill.vue
@@ -113,7 +113,12 @@ const decrementMonths = () => {
         </div>
     </td>
     <td class="px-6 py-4 d-flex flex-row justify-content-end gap-2">
-        <CustomButton v-if="! form.id" @click="addSkill" type="button" class="btn-light">Add</CustomButton>
+        <CustomButton v-if="! form.id" @click="addSkill" type="button" class="inline-flex items-center justify-center p-1 text-sm font-medium h-6 w-6 text-gray-500 bg-white border-0 border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+            <span class="sr-only">Add skill</span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-6 h-6 light-text">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+        </CustomButton>
 
         <CustomButton v-if="form.id" @click="updateSkill" type="button" class="btn-light">Save</CustomButton>
 
