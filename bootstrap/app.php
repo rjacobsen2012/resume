81a69ad5f96d95e7812972371bd76a7615b57b62
Shift <shift@laravelshift.com>
2024-03-13 11:37:03 +0000

    Re-register service providers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,11 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders()
+    ->withProviders([
+        \App\Providers\SparkServiceProvider::class,
+        \App\Providers\FortifyServiceProvider::class,
+        \App\Providers\JetstreamServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         // api: __DIR__.'/../routes/api.php',
